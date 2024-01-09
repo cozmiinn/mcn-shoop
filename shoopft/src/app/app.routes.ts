3c2270e818efe7b2d068fef1a34f4360af8e0178
Cosmin <cosmiin.mihalescull@gmail.com>
2024-01-09 23:49:26 +0200

    basics design for login and register
    after logic implementation I will come back to upgrade the design

--- shoopft/src/app/app.routes.ts
+++ shoopft/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomepageComponent} from "./components/pages/homepage/homepage.component";
+import {LoginComponent} from "./components/pages/login/login.component";
+import {RegisterComponent} from "./components/pages/register/register.component";
 
 export const routes: Routes = [
   {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
